8f7b10837f2146615923dab9e219db9a2b08dfed
JT Salisbury <crazyscouter1@gmail.com>
2020-02-29 13:12:22 -0500

    Idek2

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const app = express()
 
-const app = express();
+app.set('view engine', 'ejs')
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.get('/', function (req, res) {
+    res.render('index');
+})
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
