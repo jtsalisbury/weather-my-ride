e18cc28f2c4c827e321bf79b8a0993cd40c5862a
JT Salisbury <crazyscouter1@gmail.com>
2020-02-29 12:11:34 -0500

    Fix again

--- src/config-env.js
+++ src/config-env.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 require('dotenv').config();
 
 let apiURL = process.env.googleApiKey;
+let production = process.env.PRODUCTION;
 
 const targetPath = `./src/environments/environment.ts`;
 
 const envConfigFile = `
 export const environment = { 
+    production: ${production},
     googleApiKey: '${apiURL}'
 };`
 
